a3fc9e1f45b694f59a40c12116c37b7c8a0c1d81
neobang1337 <132597990+neobang1337@users.noreply.github.com>
2023-05-04 21:17:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿var tips = [
+	"MORGENSTRUN", 
+	"LIL PEEP", 
+	"LIL PUMP",
+	"Нейромонах Феофан",
+	"Princess Chelsea",
+	"ЗЕМФИРА",
+	"ДДТ",
+	]
+
+function getRandomTip() {
+	return tips[Math.floor(Math.random() * tips.length)];
+}
+
+document.write(getRandomTip());
+
+
+
